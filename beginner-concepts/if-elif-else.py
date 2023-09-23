52adf0d8f510d03a830e8f8785aa753a67909b5b
Ryan Python <rjeon94@gmail.com>
2023-09-23 19:23:07 -0400

    added if-elif-else

---
+++ beginner-concepts/if-elif-else.py
@@ -0,0 +1,47 @@
+# Conditional Statements:
+# use the if-elif-else structure, don't forget to have a >, < or == after the if and elif
+
+
+## 1:
+# num = 10
+# if num > 0:
+#     print("The number", num, "is less than 0")
+# elif num < 0: 
+#     print("The number", num, "is greater than 0")
+# else:
+#     print("The number is 0")
+
+## 2: 
+# You can "nest" another if statement within another like this:
+# x = 10
+# y = 20
+
+# if x > 5:
+#     if y > 10:
+#         print("x,", x, ", and y,", y, ", are greater than 5 and 10, respectively")
+#     else:
+#         print("x is greater than 5, but y is not greater than 10")
+# else: 
+#     print("x is not greater than 10")
+
+## 3: Ternary operators:
+# x = 10
+# if x > 0:
+#     result = "positive"
+# else:
+#     result = "negative"
+
+## using the above, let's "convert" it to ternary form
+# result = "positive" if x > 0 else "negative"
+# print(result)
+
+## nested ternary operators:
+# x = 10
+# result = "positive" if x > 0 else ("negative" if x < 0 else "zero")
+## so same as above, but you add a condition after the second one, and wrap that in parantheses
+
+
+
+
+
+
